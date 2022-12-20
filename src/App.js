ad51a032f3d8a47e3e52e27173c06cb1008afc78
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-20 17:36:31 +0900

    added userInputHandler

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         name: enteredName,
         id: Math.random().toString(),
       });
+      return updatedUserList;
     });
   };
 
--- src/components/Users/UserInput/UserInput.js
+++ src/components/Users/UserInput/UserInput.js
@@ -8,6 +8,11 @@ const UserInput = (props) => {
     console.log(event.target.value);
   };
 
+  const userAgeInputHandler = (event) => {
+    setEnteredValue(event.target.value);
+    console.log(event.target.value);
+  };
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
     props.onAddUser(enteredValue);
@@ -21,7 +26,7 @@ const UserInput = (props) => {
       </div>
       <div>
         <label>Age(Years)</label>
-        <input type="text"></input>
+        <input type="text" onChange={userAgeInputHandler}></input>
       </div>
       <button type="submit">Add</button>
     </form>
